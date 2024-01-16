a30259b2db3c819e9afa7f5dcc7772ff14a06fe1
Santosh Kumar Doodala <84093797+yeskaydee@users.noreply.github.com>
2024-01-16 18:31:35 +0530

    TA_Harry_Potter/help1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { CreateTodo } from './components/CreateTodo'
 import './App.css'
 import { Todos } from './components/Todos'
 
 function App() {
+  const todosArray = [
+    {
+      title: "asad",
+      description: "dsd",
+      completed: false
+    },
+    {
+      title: "asasdvd",
+      description: "dsvsdd",
+      completed: false
+    }
+  ];
 
   return (
    <div>
     <CreateTodo></CreateTodo>
-    <Todos todos={[
-      {
-        title:"asad",
-        description:"dsd",
-        completed:false
-      },
-      {title:"asad",
-      description:"dsd",
-      completed:false
-      }
-    ]}></Todos>
+    <Todos todos={todosArray}></Todos>
    </div>
   )
 }
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,18 +1,15 @@
 export function Todos(todos){
-    if (!Array.isArray(todos)) {
-        console.error('Todos is not an array:', todos);
-        return null;
-    }
+    // if (!Array.isArray(todos)) {
+    //     console.error('Todos's not an array:', todos);
+    //     return null;
+    // }
     return <div>
-        {/* <h1> Go to the gym</h1>
-        <h2>jao jao bsdk</h2>
-        <button>Mark as Done!</button> */}
-        {todos.map(function (todo){
-            return <div>  
+         {todos.map(function (todo, index){
+            return (<div key = {index}>
                 <h1>{todo.title}</h1>
                 <h2>{todo.description}</h2>
                 <button>{todo.completed==true ? "Completed" : "Mark as complete!" }</button> 
-            </div>
+            </div>)
         })}
     </div>
 
